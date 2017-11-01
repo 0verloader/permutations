de9e12fa5bdb8b5fb8e1c7633b912f6418c6403a
0verloader <31502488+0verloader@users.noreply.github.com>
2017-11-01 16:57:32 +0200

    Update permutations.py

--- permutations.py
+++ permutations.py
@@ -7,7 +7,7 @@ def table_hard_copy(table):
         new.append(i)
     return new
 
-def create_anagrams_recursive(length,curr,res):
+def create_permutations_recursive(length,curr,res):
     tmp = table_hard_copy(curr)
     if len(tmp) == length:
         print curr
@@ -16,19 +16,19 @@ def create_anagrams_recursive(length,curr,res):
         zz = table_hard_copy(tmp)
         if i not in zz:
             zz.append(i)
-            create_anagrams_recursive(length,zz,res)
+            create_permutations_recursive(length,zz,res)
     return zz
 
 
-def create_anagrams(length):
+def create_permutations(length):
     a=[]
     res=[]
     t1 = time.time()
-    create_anagrams_recursive(length,a,res)
+    create_permutations_recursive(length,a,res)
     t2 = time.time()
     print "______________________"
     print len(res),"samples"
     print t2-t1,"s."
     return res
 
-create_anagrams(int(sys.argv[1]))
+create_permutations(int(sys.argv[1]))
